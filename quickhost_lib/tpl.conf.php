65a1f0eb0a3759f8fee1660ba3cddea7631d466b
Ross t <r.tweedie@gmail.com>
2014-11-04 11:04:29 +0000

    Update tpl.conf.php

--- quickhost_lib/tpl.conf.php
+++ quickhost_lib/tpl.conf.php
@@ -9,6 +9,9 @@
      #Deny from all   # This is the original line
      Allow from all   # Changed to this line
      Satisfy all
+     
+    # New directive needed in Apache 2.4.3:
+   	Require all granted
   </Directory>
   ErrorLog "/var/log/sites/<?php echo $name ?>.local-error_log"
   CustomLog "/var/log/sites/<?php echo $name ?>.local-access_log" common
